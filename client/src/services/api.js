43854e4dcdefff8e98fb94d76dd36d399e947fcd
PrathmR <prathmeshredekar16@gmail.com>
2025-10-18 18:13:33 +0530

    sghd

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-axios.get(import.meta.env.VITE_API_URL + "/api/...");
+fetch("${import.meta.env.VITE_API_URL}/something");
 
 // const API = axios.create({
 // baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000/api',
